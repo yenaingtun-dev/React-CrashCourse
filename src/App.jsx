714cc97376971a2ffd6ce9c16e6cace9c471fc67
Ye Naing Tun <yenaingtun425@gmail.com>
2024-06-02 10:45:23 +0630

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Nav } from './components/nav';
+import { Banner } from './components/banner';
 
 const App = () => {
   return (
     <>
       <Nav></Nav>
+      <Banner></Banner>
     </>
   )
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,4 @@
 export const Nav = () => {
-
   return (
     <header className="bg-white">
       <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
